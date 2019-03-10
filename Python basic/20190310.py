4c1fbd4844143eb73c76b571e5edf7ff67a508b5
daisy548 <41964005+daisy548@users.noreply.github.com>
2019-03-11 00:39:51 +0900

    Add files via upload

--- Python basic/20190310.py
+++ Python basic/20190310.py
@@ -1,2 +1,53 @@
-print("hello")
-a = 10
+# 참조 사이트 : https://wikidocs.net/13
+
+# 4가지 방법을 통한 따옴표 표시
+
+food = "Python's favorite food is perl"
+say = '"Python is very easy." he says.'
+food2 = 'Python\'s favorite food is perl'
+say2 = "\"Python is very easy.\" he says."
+
+#print(say2)
+
+# 여러 줄의 문자열을 변수에 대입
+multiline = "Life is too short\nYou need python"
+multiline2 = '''Life is too short
+You need python'''
+multiline3 = """Life is too short
+You need python"""
+
+#print(multiline3)
+
+# 문자열 연산
+
+head = "Python"
+tail = " is fun!"
+fox = head + tail
+#print(fox)
+
+a = "python"
+b = a*2
+#print(b)
+
+
+# 문자열 곱하기 응용 (이쁘게 프린트)
+
+#print("="*50)
+#print("My Program")
+#print("="*50)
+
+
+# 문자열 길이 구하기
+
+length = "Life is too short"
+#print(len(length))
+
+# 문자열 인덱싱과 슬라이싱
+
+indexing = "Life is too short, You need Python"
+#print(indexing[3])
+#print(indexing[-1])
+#print(indexing[-3])
+#print(indexing[0])
+#print(indexing[-0])
+
